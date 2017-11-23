753255f5ceabadea6ca0c82c622ec761631c3d7e
Slava <Slava@MININT-J0047CG>
2017-11-23 22:21:25 +0100

    Add project files.

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [TestCase("filewithgoodextension.SLF", true)]
+        [TestCase("filewithgoodextension.slf", true)]
+        [TestCase("filewithbadextension.foo", false)]
+        public void IsValidFileName_VariousExtensions_ChecksThem(string file, bool expected)
+        {
+            LogAnalyzer logAnalyzer = new LogAnalyzer(null);
+
+            bool result = logAnalyzer.IsValidLogFileName(file);
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [Test]
+        public void IsValidFileName_TooShort_CallsWebService()
+        {
+            FakeWebService fakeWebService = new FakeWebService();
+            LogAnalyzer logAnalyzer = new LogAnalyzer(fakeWebService);
+
+            logAnalyzer.Analyze("123.ext");
+
+            StringAssert.Contains("Filename too short:123.ext", fakeWebService.LastError);
+        }
+
+        public class FakeWebService : IWebService
+        {
+            public String LastError;
+            public void LogError(string message)
+            {
+                LastError = message;
+            }
+        }
+
+
+        /*[Test]
+        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer
+            .IsValidLogFileName("filewithgoodextension.slf");
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result =
+            analyzer
+            .IsValidLogFileName("filewithgoodextension.SLF");
+
+            Assert.True(result);
+        }
+        */
+    }
+}
---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+
+        private IWebService mWebService;
+
+        public LogAnalyzer(IWebService webService)
+        {
+            mWebService = webService;
+        }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Analyze(string filename)
+        {
+            if (filename.Length < 8)
+                mWebService.LogError("Filename too short:" + filename);
+        }
+    }
+}
