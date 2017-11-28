119df072002de2caf3c7f7a0fda55ade27da0196
ccppcsh <viacheslav.lylin@gmail.com>
2017-11-28 22:01:48 +0100

    Ch5. Testing of events

---
+++ LogAn.UnitTests/PresenterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    class PresenterTest
+    {
+        [Test]
+        public void ctor_WhenViewIsLoaded_CallsViewRender()
+        {
+            var mockView = Substitute.For<IView>();
+
+            Presenter presenter = new Presenter(mockView);
+
+            mockView.Loaded += Raise.Event<Action>();
+
+            mockView.Received()
+                .Render(Arg.Is<String>(s => s.Contains("Hello World")));
+        }
+    }
+}
---
+++ LogAn/Presenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    public class Presenter
+    {
+        private readonly IView _view;
+        public Presenter(IView view)
+        {
+            _view = view;
+            this._view.Loaded += OnLoaded;
+        }
+        private void OnLoaded()
+        {
+            _view.Render("Hello World");
+        }
+    }
+
+    public interface IView
+    {
+        event Action Loaded;
+        void Render(string text);
+    }
+}
